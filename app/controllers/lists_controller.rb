8727b206d301f4b3f5db3b58296c104118e2a37a
Maria Cassino <maria.cassino@gmail.com>
2016-07-18 18:11:32 -0400

    optimized db querying in lists#destroy method

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,13 @@
 class ListsController < ApplicationController
 
-  def parsed_data
-    incoming_data = request.body.read
-    begin
-      JSON.parse incoming_data
-    rescue
-      halt 400, "Request not JSON formatted"
-    end
-  end
+  # def parsed_data
+  #   incoming_data = request.body.read
+  #   begin
+  #     JSON.parse incoming_data
+  #   rescue
+  #     halt 400, "Request not JSON formatted"
+  #   end
+  # end
 
 
   def index
@@ -16,21 +16,17 @@ class ListsController < ApplicationController
   end
 
 
-
   def show
   @list = List.where(id: params[:id], home_id: params[:home_id])
   end
 
 
-
   def new
-
   end
 
 
   def create
-      @list = List.create(name: parsed_data["name"], item: parsed_data["item"],
-              user_id: user.id, home_id: home.id)
+      @list = List.new(list_params)
       if @list.save
         render :show
       else
@@ -39,17 +35,19 @@ class ListsController < ApplicationController
   end
 
 
-
-
   def edit
   end
+
 
   def update
   end
 
+
   def destroy
-    @home = Home.find_by(params[:id])
-    @home.lists.find_by(name: parsed_data["name"]).delete
+    # @home = Home.find_by(params[:id])
+    # @home.lists.find_by(name: parsed_data["name"]).delete
+    @list = List.where(id: params[:id], home_id: params[:home_id])
+    @list.destroy 
   end
 
 
@@ -57,7 +55,7 @@ class ListsController < ApplicationController
 private
 
   def approved_params
-    params.require(:list).permit(:name, :item)
+    params.require(:list).permit(:name, :item, :user_id, :home_id)
   end
 
 end
