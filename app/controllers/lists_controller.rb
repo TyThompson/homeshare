cbd6bd29b32c122e46df691f4169a7f442c39825
Maria Cassino <maria.cassino@gmail.com>
2016-07-18 16:41:07 -0400

    created initial jbuilder object for Lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,59 +1,63 @@
 class ListsController < ApplicationController
 
-def parsed_data
-  incoming_data = request.body.read
-  begin
-    JSON.parse incoming_data
-  rescue
-    halt 400, "Request not JSON formatted"
-  end
-end
-
-
-def index
-  @home = Home.find(params[:id])
-  @lists = @home.lists.find_by(name: parsed_data["name"])
-end
-
-
-
-def show
-   @home = Home.find(params[:id])
-   @list = @home.lists.find_by(name: parsed_data["name"])
-end
-
-
-
-def new
-
-end
-
-
-def create
-    @list = List.create(name: parsed_data["name"], item: parsed_data["item"],
-            user_id: user.id, home_id: home.id)
-    if @list.save
-      render :show
-    else
-      render @list.errors
+  def parsed_data
+    incoming_data = request.body.read
+    begin
+      JSON.parse incoming_data
+    rescue
+      halt 400, "Request not JSON formatted"
     end
   end
-end
+
+
+  def index
+    @home = Home.find_by(params[:id])
+    @lists = @home.lists
+  end
+
+
+
+  def show
+  @list = List.where(id: params[:id], home_id: params[:home_id])
+  end
+
+
+
+  def new
+
+  end
+
+
+  def create
+      @list = List.create(name: parsed_data["name"], item: parsed_data["item"],
+              user_id: user.id, home_id: home.id)
+      if @list.save
+        render :show
+      else
+        render @list.errors
+      end
+  end
 
 
 
 
-def edit
-end
+  def edit
+  end
 
-def update
-end
+  def update
+  end
 
-def destroy
-  @home = Home.find(params[:id])
-  @home.lists.find_by(name: parsed_data["name"]).delete
-end
+  def destroy
+    @home = Home.find_by(params[:id])
+    @home.lists.find_by(name: parsed_data["name"]).delete
+  end
 
 
+
+private
+
+  def approved_params
+    params.require(:list).permit(:name, :item, :user_id, :home_id)
+  end
 
 end
