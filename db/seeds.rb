971fbe3e86ceedc55df3aaa638259eccbfb06f50
Maria Cassino <maria.cassino@gmail.com>
2016-07-18 10:59:33 -0400

    revised Seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,46 +7,47 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+
 #Users
 maria = User.create(:email => 'maria@example.com', :password => 'password')
 ty = User.create(:email => 'ty@example.com', :password => 'password')
 
 20.times do User.create(:email => Faker::Internet.email,
-  :password => Faker::Internet.password, :admin => false)
+  :password => Faker::Internet.password)
 end
 
 
 #Homes
-marias_home = Home.create(:name => "Maria's house", :user => maria)
+marias_home = Home.create(:name => "Maria's house", :user_id => maria.id)
 
-tys_home = Home.create(:name => "Ty's house", :user => ty)
+tys_home = Home.create(:name => "Ty's house", :user_id => ty.id)
 
 
 #Chores
-5.times do Chore.create(:user => maria, :home => marias_home, :name => Faker::Lorem.sentence,
+5.times do Chore.create(:user => maria, :home_id => marias_home.id, :name => Faker::Lorem.sentence,
   :value => Faker::Number.between(1, 100) )
 end
 
-5.times do Chore.create(:user => ty, :home => tys_home, :name => Faker::Lorem.sentence,
+5.times do Chore.create(:user => ty, :home_id => tys_home.id, :name => Faker::Lorem.sentence,
   :value => Faker::Number.between(1, 100))
 end
 
 
 #Bills
-5.times do Bill.create(:user => maria, :home => marias_home, :name => Faker::Lorem.sentence,
+5.times do Bill.create(:user => maria, :home_id => marias_home.id, :name => Faker::Lorem.sentence,
   :amount => Faker::Commerce.price)
 end
 
-5.times do Bill.create(:user => ty, :home => tys_home, :name => Faker::Lorem.sentence,
+5.times do Bill.create(:user => ty, :home_id => tys_home.id, :name => Faker::Lorem.sentence,
   :amount => Faker::Commerce.price)
 end
 
 
 #Shopping Lists
-5.times do Shopping_List.create(:user => maria, :home => marias_home, :name => Faker::Lorem.sentence,
+5.times do List.create(:user_id => maria.id, :home_id => marias_home.id, :name => Faker::Lorem.sentence,
   :item => Faker::Lorem.sentence)
 end
 
-5.times do Shopping_List.create(:user => ty, :home => tys_home, :name => Faker::Lorem.sentence,
-  :amount => Faker::Lorem.sentence)
+5.times do List.create(:user_id => ty.id, :home_id => tys_home.id, :name => Faker::Lorem.sentence,
+  :item => Faker::Lorem.sentence)
 end
