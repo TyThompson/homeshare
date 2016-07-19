06ccbfcaa0f7dcf995c3ff93235cd6dd6fd335e2
tythompson <tythompson@gmail.com>
2016-07-19 09:32:44 -0400

    enable emailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
     def check_user
       # render 'not_allowed' unless @question.user_id == params[:user_id]
+    end
+
+    def emailer
+      # contact = Watchlist.where("price < ?", @pet.price).where(species: @pet.species)
+        contact.each do |p|
+          UserNotifier.send_email(p.user).deliver
+        end
     end
 end
