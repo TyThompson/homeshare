be4cfede938a74d7d1f6b80859d95d0134c12047
tythompson <tythompson@gmail.com>
2016-07-20 10:06:14 -0400

    fixed CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 
     def check_user
       # render 'not_allowed' unless @question.user_id == params[:user_id]
--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,10 @@ Bundler.require(*Rails.groups)
 module Homeshare
   class Application < Rails::Application
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
      allow do
        origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :options]
+       resource '*', :headers => :any, :methods => :any
      end
 
      ActionMailer::Base.smtp_settings = {
