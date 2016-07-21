3c0f1e5e6f75c77ebc4be835e1250cb5eeda5eff
Maria Cassino <maria.cassino@gmail.com>
2016-07-21 09:54:35 -0400

    added clearer, more specific column names to Chores table

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 20160720180815) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160721134622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -14,16 +26,19 @@ ActiveRecord::Schema.define(version: 20160720180815) do
   end
 
   create_table "chores", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "home_id",      null: false
-    t.string   "name",         null: false
+    t.integer  "chore_creator_id"
+    t.integer  "home_id",            null: false
+    t.string   "name",               null: false
     t.text     "description"
     t.float    "bill_value"
-    t.integer  "value",        null: false
+    t.integer  "value",              null: false
     t.integer  "votes"
     t.integer  "thumbs_up"
-    t.datetime "created_at",   null: false
+    t.datetime "created_at",         null: false
     t.datetime "completed_at"
+    t.integer  "chore_completer_id"
+    t.integer  "chore_assignee_id"
+    t.string   "avatar"
   end
 
   create_table "homes", force: :cascade do |t|
