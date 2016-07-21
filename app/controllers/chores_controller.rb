167e3475b30186ef5725d3c2376759619379f0ec
Maria Cassino <maria.cassino@gmail.com>
2016-07-21 09:43:20 -0400

    added authenticate_user before_action to Chores controller

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,6 +1,7 @@
 class ChoresController < ApplicationController
   # before_action :check_login, except: [:show, :index, :search]
   before_action :set_chore, except: [:create, :index]
+  before_action :authenticate_user!
   # before_action :check_user, only: [:update, :destroy]
 
 
