93ad7e7d0e1b03c8cd0e6c60464020529a4beb68
Maria Cassino <maria.cassino@gmail.com>
2016-07-22 12:05:31 -0400

    set chore_xp to less than or equal to 100

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,6 +1,8 @@
 class Chore < ActiveRecord::Base
   validates :name, presence: true
   validates :home_id, presence: true
+  validates_numericality_of :chore_xp, less_than_or_equal_to: 100 #:in => 1..99
+
 
   belongs_to :home
   belongs_to :chore_creator, class_name: 'User'
