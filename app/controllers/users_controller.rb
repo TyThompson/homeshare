391d421705ff8b7f872b4cb91da9ded65aa26efe
tythompson <tythompson@gmail.com>
2016-07-25 16:51:56 -0400

    added exp
    added exp formula

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ before_action :set_homes, except: [:create, :index, :destroy]
 
   def show
     @total_exp = Chore.where(chore_completer_id: params[:id]).pluck(:chore_xp).sum
+    user_exp = @total_exp
+    exp = 100.0
+    @level = 0
+    until exp > user_exp
+      @level += 1
+      exp = exp * 2.0
+    end
+    @percent = (user_exp/exp)
   end
 
   def index
--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,5 +1,7 @@
 json.user do
   json.housemate @user
   json.total_exp @total_exp
+  json.level @level
+  json.percent @percent
   json.homes @homes
 end
