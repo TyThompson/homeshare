d330038cf23b0d3a5c3f6cca3bc22fce16329056
Maria Cassino <maria.cassino@gmail.com>
2016-07-29 11:18:20 -0400

    prototype for paypal http request

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -17,6 +17,36 @@ class PaymentsController < ApplicationController
   end
 
 
+
+  def paypal
+    @amount = Payment.find_by(sender_paypal_email: current_user.email).amount
+    request = HTTParty.post("https://svcs.sandbox.paypal.com/AdaptivePayments/Pay",
+      headers: {"X-PAYPAL-SECURITY-USERID": "maria.cassino-facilitator_api1.gmail.com"
+                "X-PAYPAL-SECURITY-PASSWORD": "U9FL2MK962DKPXMR"
+                "X-PAYPAL-SECURITY-SIGNATURE": "AFcWxV21C7fd0v3bYYYRCpSSRl31A2tjSJXfuAz3het2TLAiz2uCt1eN"
+                "X-PAYPAL-REQUEST-DATA-FORMAT": "JSON"
+                "X-PAYPAL-RESPONSE-DATA-FORMAT": "JSON"
+                "X-PAYPAL-APPLICATION-ID": "APP-80W284485P519543T"
+                },
+      data:   {actionType: "PAY",
+                currencyCode: "USD",
+                receiverList: {
+                  receiver:[
+                             {amount: @amount,
+                              email: "maria.cassino@gmail.com"}
+                            ]
+                      },
+              returnUrl: "http://www.example.com/success.html",
+              cancelUrl: "http://www.example.com/failure.html",
+              requestEnvelope:{
+                errorLanguage: "en_US",
+                detailLevel: "ReturnAll"
+                })
+  end
+
+
+
+
   def sent_payments
     @payments = Payment.where(sender_paypal_email: current_user.paypal_email)
   end
