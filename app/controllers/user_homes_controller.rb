48b557e57c706dc47a602f0eb842abc7094fc251
tythompson <tythompson@gmail.com>
2016-08-02 10:19:22 -0400

    setting up users joining /leaving homes

--- app/controllers/user_homes_controller.rb
+++ app/controllers/user_homes_controller.rb
@@ -8,12 +8,13 @@ before_action :set_user_home, except: [:create, :index]
 
 # show list of each user in that home along with their xp and level for that home
   def index
-    @userhomes = UserHome.where(home_id: params[:home_id])
+    @userhomes = UserHome.where(home_id: params[:home_id].to_i)
   end
 
 # user joins a home
   def create
     @userhome = UserHome.new(user_home_params)
+    @userhome.user_id = current_user.id
     if @userhome.save
       render :show, status: 201
     else
@@ -38,7 +39,7 @@ before_action :set_user_home, except: [:create, :index]
   end
 
   def user_home_params
-    params.require(:user_homes).permit(:home_id, :id)
+    params.require(:user_homes).permit(:home_id)
   end
 
 end
