304f699fd90935eac285611bf70792b4289ee519
tythompson <tythompson@gmail.com>
2016-08-02 13:41:24 -0400

    took out question marks

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -66,7 +66,7 @@ class HomesController < ApplicationController
     begin
       UserHome.exists?(user_id: current_user.id, home_id: @home.id)
     rescue
-      render json: "user_in_home? method error"
+      render json: "user_in_home method error"
     end
   end
 
@@ -80,7 +80,7 @@ class HomesController < ApplicationController
         return false
       end
     rescue
-      render json: "friend_exists? method error"
+      render json: "friend_exists method error"
     end
   end
 
